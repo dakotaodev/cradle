ef800a6726c7bcd389c5e328c6466bfc205e5435
Dakota <dakota.o.dev@gmail.com>
2026-09-18 21:27:28 -0700

    feat: update SQL migration for babies table with constraints and default values

--- migrations/00001-create-babies.up.sql
+++ migrations/00001-create-babies.up.sql
@@ -1,8 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
-CREATE TABLE IF NOT EXISTS babies (
+CREATE TABLE babies (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     full_name TEXT NOT NULL,
     birth_date DATE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    
+    CONSTRAINT full_name_empty CHECK (length(TRIM(full_name))>0)
 );
