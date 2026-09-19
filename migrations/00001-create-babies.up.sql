8249027ee71233c4d65de633c478237bbdce2432
Dakota <dakota.o.dev@gmail.com>
2026-09-18 21:15:06 -0700

    feat: add SQL migration files for babies table creation and deletion

---
+++ migrations/00001-create-babies.up.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS babies (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    full_name TEXT NOT NULL,
+    birth_date DATE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
